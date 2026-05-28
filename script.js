70a56ec08bf028d04ff1a22948978e1364d98e86
rosetan88 <orirocket66@gmail.com>
2026-05-28 14:23:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Close top app bar
-document.querySelector(".close-btn").addEventListener("click", function(){
-  document.querySelector(".app-bar").style.display = "none";
-});
-
-// Category active button effect
-document.querySelectorAll(".category-tabs button").forEach(function(btn){
-  btn.addEventListener("click", function(){
-    document.querySelectorAll(".category-tabs button").forEach(function(b){
-      b.classList.remove("active");
-    });
-    btn.classList.add("active");
-  });
-});
+document.querySelector(".close-btn").addEventListener("click",()=>{document.querySelector(".app-bar").style.display="none";document.querySelector(".tabs").style.top="0";});
